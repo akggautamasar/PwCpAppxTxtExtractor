205bac4d934ff71c0e76b6ab96a73c9e926468e0
akggautamasar <Worksbeyondworks@gmail.com>
2025-05-25 01:18:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "22649259"
+api_hash = "545169590ffbfe0bf8bade55e3a1cfde"
+bot_token = ""
+auth_users = [6221765779]
